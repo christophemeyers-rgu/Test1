cca5b632a72188d6b65728f43aa90425b5fdc705
Christophe <christophe.meyers@outlook.com>
2016-03-03 11:30:09 +0000

    work in progress

--- mail.php
+++ mail.php
@@ -6,13 +6,18 @@
  * Time: 20:52
  */
 
-
+//setting some variables with form values
 $name = $_POST["name"];
 $password = $_POST["password"];
 $email = $_POST["email"];
 
+//email receiver
 $to = $_POST["email"];
+
+//email subject
 $subject = "Befriend A Child - Survey Login";
+
+//email content
 $txt = "Dear $name,
         <br><br>
         An account has been set up in your name.
@@ -30,11 +35,14 @@ $txt = "Dear $name,
         <br><br>
         The Befriend A Child Team";
 
+//currently not used because I'm using the mail(to,subject,content) rather than mail(to,subject,content,headers) version
 $headers = "From: christophe.meyers.312@gmail.com" . "\r\n" .
     "CC: somebodyelse@example.com";
 
-echo mail($to,$subject,$txt);
+//function to send email to (receiver, subject, content)
+mail($to,$subject,$txt);
 
+//print email content to display whether the content is as expected
 echo $txt;
 ?>
 
