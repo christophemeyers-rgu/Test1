f048354be3af8e93892c2c1eb9fc4d57dd0b8129
Christophe <christophe.meyers@outlook.com>
2016-03-06 20:41:34 +0000

    work in progress

--- formsubmitted.php
+++ formsubmitted.php
@@ -30,45 +30,45 @@
     //email content
     $txt = "Dear $name,
             <br><br>
-    An account has been set up in your name.
-    <br>
-    If you would like to fill out a survey concerning your experience with Befriend A Child,
-    please follow
-    <a href='http://befriendachildtestsurvey.azurewebsites.net/volunteerlogin.html'>this link</a>
-    and login with:
-    <br><br>
-    Username: $email
-    <br>
-    Password: $password
-    <br><br>
-    You will be able to change your password once logged in.
-    <br><br>
-    King Regards,
-    <br><br>
-    The Befriend A Child Team";
+            An account has been set up in your name.
+            <br>
+            If you would like to fill out a survey concerning your experience with Befriend A Child,
+            please follow
+            <a href='http://befriendachildtestsurvey.azurewebsites.net/volunteerlogin.html'>this link</a>
+            and login with:
+            <br><br>
+            Username: $email
+            <br>
+            Password: $password
+            <br><br>
+            You will be able to change your password once logged in.
+            <br><br>
+            King Regards,
+            <br><br>
+            The Befriend A Child Team";
 
 
 
     require_once 'swiftmailer/lib/swift_required.php';
 
-    $transport = Swift_SmtpTransport::newInstance('smtp.gmail.com', 465, "ssl")
-    ->setUsername('christophe.meyers.312@gmail.com')
-    ->setPassword('AnnachengAddress');
+    $transporter = Swift_SmtpTransport::newInstance('smtp.gmail.com', 465, "ssl")
+        ->setUsername('christophe.meyers.312@gmail.com')
+        ->setPassword('AnnachengAddress');
 
-    $mailer = Swift_Mailer::newInstance($transport);
+    $mailer = Swift_Mailer::newInstance($transporter);
 
     $message = Swift_Message::newInstance('Befriend A Child Test Mail')
-    ->setFrom(array('christophe.meyers.312@gmail.com' => 'ABC'))
-    ->setTo(array('estima.meyers@gmail.com'))
-    ->setBody('This is a test mail.');
+        ->setFrom(array('christophe.meyers.312@gmail.com' => 'Christophe Meyers'))
+        ->setTo(array($email => $name))
+        ->setBody($txt);
 
     $result = $mailer->send($message);
 
     if($mailer->send($message) == 1){
-    echo 'send ok';
+        echo 'send ok';
     }
     else {
-    echo 'send error';
+        echo 'send error';
     }
 
     echo function_exists('proc_open') ? "Yep, that will work" : "Sorry, that won't work";
