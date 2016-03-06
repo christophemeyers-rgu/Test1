09f590432b76b76468d772a769e0cb40e119cfb8
Christophe <christophe.meyers@outlook.com>
2016-03-06 20:56:26 +0000

    work in progress

--- formsubmitted.php
+++ formsubmitted.php
@@ -14,8 +14,7 @@
      * Time: 20:52
      */
 
-    //ini_set("SMTP", "smtp.gmail.com");
-    //ini_set("sendmail_from", "christophe.meyers.312@gmail.com");
+
 
 
 
@@ -29,7 +28,7 @@
 
     //email content
     $txt = "Dear $name,
-            <br><br>
+            \n\n
             An account has been set up in your name.
             <br>
             If you would like to fill out a survey concerning your experience with Befriend A Child,
@@ -72,74 +71,7 @@
     }
 
     echo function_exists('proc_open') ? "Yep, that will work" : "Sorry, that won't work";
-    //email receiver
 
-
-
-
-    /*
-    //currently not used because I'm using the mail(to,subject,content) rather than mail(to,subject,content,headers) version
-    $headers = "From: christophe.meyers.312@gmail.com" . "\r\n" .
-    "CC: somebodyelse@example.com";
-
-    //function to send email to (receiver, subject, content)
-    mail($email,$subject,$txt);*/
-
-
-    /*require_once("Mail.php");
-
-    $from = "<christophe.meyers.312@gmail.com>";
-    $to = "<$email>";
-    $subject = "Test Befriend A Child Mail";
-    $body = "Dear $name,
-    <br><br>
-    An account has been set up in your name.
-    <br>
-    If you would like to fill out a survey concerning your experience with Befriend A Child,
-    please follow
-    <a href='http://befriendachildtestsurvey.azurewebsites.net/volunteerlogin.html'>this link</a>
-    and login with:
-    <br><br>
-    Username: $email
-    <br>
-    Password: $password
-    <br><br>
-    You will be able to change your password once logged in.
-    <br><br>
-    King Regards,
-    <br><br>
-    The Befriend A Child Team";
-
-    $host = "smtp.gmail.com";
-    $username = "christophe.meyers.312@gmail.com";
-    $password = "AnnachengAddress";
-    $port = "465";
-
-    $headers = array(
-    'From' => $from,
-    'To' => $to,
-    'Subject' => $subject
-    );
-
-    $smtp = Mail::factory('smtp', array (
-    'host' => $host,
-    "port" => $port,
-    'auth' => true,
-    'username' => $username,
-    'password' => $password));
-
-    $mail = $smtp->send($to, $headers, $body);
-
-    if ( PEAR::isError($mail) ) {
-    echo("<p>Error sending mail:<br/>" . $mail->getMessage() . "</p>");
-    } else {
-    echo("<p>Message sent.</p>");
-    }*/
-
-
-
-    //print email content to display whether the content is as expected
-    //echo $txt;
     ?>
 
 
