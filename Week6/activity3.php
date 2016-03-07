e64463373c0b5f84c396c6adf2a74b92dadaf3b0
Christophe <christophe.meyers@outlook.com>
2016-03-07 10:03:11 +0000

    Week 6 Lab

---
+++ Week6/activity3.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+
+    <form action="activity3.html" method="post">
+
+        <select name="wantedgood">
+            <option value="specs">Specs</option>
+            <option value="mugs">Mugs</option>
+            <option value="sausagerolls">Sausage Rolls</option>
+        </select>
+        <input type="submit">
+    </form>
+
+
+    <?php
+
+    $wantedgood = $_POST["wantedgood"];
+
+    switch($wantedgood){
+        case "specs":
+            echo "You have to be 16 to buy specs.";
+            break;
+        case "mugs":
+            echo "You have to be 18 to buy mugs.";
+            break;
+        case "sausagerolls":
+            echo "You have to be 21 to buy sausage rolls.";
+            break;
+        default:
+            echo "You do what you want.";
+    }
+
+    ?>
+</body>
+</html>
