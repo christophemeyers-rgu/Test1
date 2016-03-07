ec8616e3750d5ff532569ca6e6bb836060cfcf1e
Christophe <christophe.meyers@outlook.com>
2016-03-07 10:26:11 +0000

    Week 6 Lab

---
+++ Week6/activity5.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+    <?php
+        $items = array("specs", "mugs", "sausage rolls");
+
+        for($i = 0; $i<30; $i++){
+            print "<p>On the $i";
+            switch($i){
+                case 1:
+                    print "st";
+                    break;
+                case 2:
+                    print "nd";
+                    break;
+                case 21:
+                    print "st";
+                    break;
+                case 22:
+                    print "nd";
+                    break;
+                default:
+                    print "th";
+            }
+            print " of the month ";
+
+            if($i%2==0){
+                print "specs ";
+                if($i%3==0){
+                    print " and mugs ";
+                }
+                if($i%4==0){
+                    print " and sausage rolls ";
+                }
+            }
+            elseif($i%3==0){
+                print "mugs ";
+            }
+
+            print "are available.</p>";
+        }
+
+
+    ?>
+</body>
+</html>
