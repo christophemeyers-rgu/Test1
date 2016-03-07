00f3c77e65820663861a17851e6b5c656076ffc4
Christophe <christophe.meyers@outlook.com>
2016-03-07 09:43:14 +0000

    Week 6 Lab

---
+++ Week6/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+    <head>
+
+    </head>
+    <body>
+
+        <?php
+        /**
+         * Created by PhpStorm.
+         * User: Christophe
+         * Date: 07/03/2016
+         * Time: 09:41
+         */
+
+        echo "Hello World";
+
+        ?>
+
+    </body>
+
+</html>
+
+
+
