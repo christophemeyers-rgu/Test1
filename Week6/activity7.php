04b301f628a249d31b983d1605fb24098f5b3c38
Christophe <christophe.meyers@outlook.com>
2016-03-08 09:40:29 +0000

    Week 6 Lab

---
+++ Week6/activity7.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+
+    <?php
+
+    $people = array();
+    array_push($people, "Chris");
+    array_push($people, "Pedro");
+    array_push($people, "Iain");
+    array_push($people, "Kyle");
+    array_push($people, "Marc");
+    array_push($people, "Patrick");
+    array_push($people, "Somto");
+    array_push($people, "Josh");
+    array_push($people, "Dinesh");
+    array_push($people, "Chudi");
+    array_push($people, "Shahid");
+    array_push($people, "David");
+    array_push($people, "Mike");
+
+    sort($people);
+
+    $draw = rand(0,count($people));
+
+    $winner = strtoupper($people[$draw]);
+    unset($people[$draw]);
+
+    print "<p>" . strtoupper($winner);
+
+    print " has won all the specs.";
+
+
+    $draw = rand(0,count($people));
+
+    $winner = strtoupper($people[$draw]);
+    unset($people[$draw]);
+
+    print "<p>" . strtoupper($winner);
+
+    print " has won all the mugs.";
+
+
+    $draw = rand(0,count($people));
+
+    $winner = strtoupper($people[$draw]);
+    unset($people[$draw]);
+
+    print "<p>" . strtoupper($winner);
+
+    print " has won all the sausage rolls.";
+
+
+
+
+
+
+    ?>
+
+
+</body>
+</html>
