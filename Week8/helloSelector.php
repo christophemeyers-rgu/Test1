0f144be0ed0a76c3acd01fc095b766fd7c54346d
Christophe <christophe.meyers@outlook.com>
2016-03-21 10:36:04 +0000

    Week 7 lab

---
+++ Week8/helloSelector.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+    <p>
+        Where would you like to say hello?
+    </p>
+    <a href="helloPrinter.php?planet=Earth">Earth</a><br>
+    <a href="helloPrinter.php?planet=Mars">Mars</a><br>
+    <a href="helloPrinter.php?planet=Uranus">Uranus</a><br>
+
+</body>
+</html>
