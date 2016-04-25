0ac19f40fd8d933225a1131f62cafabe6a6d4908
christophemeyers-rgu <christophe.meyers@outlook.com>
2016-04-25 09:51:37 +0100

    In class Mock Exam

---
+++ MockLab/index.php
@@ -0,0 +1,76 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 1511363
+ * Date: 25/04/2016
+ * Time: 09:50
+ */
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+
+    <link href='https://fonts.googleapis.com/css?family=Roboto:400,300' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="assets/css/unsemantic-grid-responsive-tablet.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1">
+
+    <title>Index</title>
+</head>
+<body>
+
+<header>
+    <img src="assets/images/logo.png" width="100px" class="TextWrap">
+    <p id="BoldTitle">BugTracker</p>
+    <p id="NormalTitle">Keeping Track of all the pesky little bugs</p>
+
+</header>
+
+<nav>
+</nav>
+
+
+<main>
+
+    <section class="grid-25 tablet-grid-33 mobile-grid-100" id="Sidebar">
+
+        <ul>
+            <li>
+                <a href="showbugs.php">All Bugs</a>
+            </li>
+            <li>
+                <a href="bugs.html">Android Bugs</a>
+            </li>
+            <li>
+                <a href="bugs.html">iOS Bugs</a>
+            </li>
+            <li>
+                <a href="bugs.html">Windows Bugs</a>
+            </li>
+            <li>
+                <a href="addbugs.php">Insert Bug</a>
+            </li>
+        </ul>
+
+    </section>
+
+    <section class="grid-75 tablet-grid-66 mobile-grid-100" id="MainSection">
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec porta pharetra lacinia. Curabitur mauris neque, finibus eget faucibus eget, condimentum pulvinar augue. Maecenas facilisis, ex vitae fermentum pretium, sem ligula tincidunt neque, vitae accumsan lorem leo a urna. Duis faucibus, sapien vitae dapibus hendrerit, mauris lectus hendrerit purus, a pulvinar nisi augue egestas ipsum. Proin laoreet eleifend justo, non sagittis orci ornare et. Phasellus tempus neque nec tempor porttitor. Cras ultricies tellus odio, sit amet placerat odio pretium in. Aenean posuere aliquam leo id laoreet. Etiam tristique urna felis, a auctor orci blandit eget. Pellentesque nec lacus orci. Praesent ac justo arcu. Sed rhoncus ornare enim, ut fringilla ligula tempor sit amet.
+        </p>
+    </section>
+
+
+
+
+</main>
+
+<footer>
+    <p>Designed by Christophe Meyers, 2016.</p>
+</footer>
+
+</body>
+</html>
